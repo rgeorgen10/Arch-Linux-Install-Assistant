30e980f800d96430ba9fedb2312d9cf97cf774a4
Riley Georgen <rgeorgen10@gmail.com>
2026-04-26 21:20:51 -0600

    Add green(), white(), and printWelcome() functions

---
+++ helper-functions.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void green() {
+    printf("\033[0;32m");
+}
+
+void white() {
+    printf("\033[0m");
+}
+
+void clear() {
+    system("clear");
+}
+
+void printWelcome() {
+    green();
+    printf("|----------------------------------------------------| \n");
+    printf("|            Arch Linux Install Assistant            | \n");
+    printf("|          --------------------------------          | \n");
+    printf("|      This script is designed to assist in the      | \n");
+    printf("|    installation of Arch Linux. This script will    | \n");
+    printf("|           assist with the following:               | \n");
+    printf("| 1) Formatting Disks                                | \n");
+    printf("| 2) Installing base system                          | \n");
+    printf("| 3) Configuring base system                         | \n");
+    printf("| 4) Installing a bootloader (GRUB)                  | \n");
+    printf("| 5) Adding Sudo and Users                           | \n");
+    printf("| 6) Installing a Display Server                     | \n");
+    printf("| 7) Installing a Display Manager                    | \n");
+    printf("| 8) Installing a Desktop Environment                | \n");
+    printf("|----------------------------------------------------| \n");
+    white();
+}
--- main-script.c
+++ main-script.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <helper-functions.h>
+#include "helper-functions.h"
 
 int main() {
-    printf("%s", "Installing VLC \n");
-    system("sudo pacman -S vlc --noconfirm");
-    printf("%s", "Done installing vlc");
+    printWelcome();    // Print Welcome Text and Script Information
     return 0;
 }

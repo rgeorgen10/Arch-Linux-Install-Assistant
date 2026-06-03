12751088cbad5abcd1f40dc1ca0e4923e0aa6a59
Riley Georgen <rgeorgen10@gmail.com>
2026-06-03 11:51:24 -0600

    fixed yay install

--- main-script.c
+++ main-script.c
@@ -412,7 +412,7 @@ addUser:
         system("arch-chroot /mnt useradd -mG wheel temp");
         system("arch-chroot /mnt pacman -S --needed --noconfirm git base-devel");
         system("echo 'temp:passwd' | arch-chroot /mnt chpasswd");
-        system("arch-chroot /mnt sudo -i -u temp cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && sudo -i -u temp makepkg -s");
+        system("arch-chroot /mnt sudo -i -u temp git clone https://aur.archlinux.org/yay.git /home/temp/yay && arch-chroot /mnt sudo -i -u temp sh -c 'cd /home/temp/yay && makepkg -s'");
         system("arch-chroot /mnt pacman -U /home/temp/yay/*.pkg.tar.zst");
         system("arch-chroot /mnt rm -r /home/temp");
         system("arch-chroot /mnt userdel temp");
